76b7471b123b29ebf7854849c1b70658327c87aa
Muhammad Aswan <muhaswan190897@gmail.com>
2022-05-25 20:13:36 +0800

    upload laracamp

---
+++ database/migrations/2022_04_27_030447_create_camp_benefits_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateCampBenefitsTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('camp_benefits', function (Blueprint $table) {
+            $table->id();
+            // 1st method 
+            // $table->unsignedBigIntegerbigInteger('camp_id');
+            // $table->foreign('camp_id')->references('id')->on('camps')->onDelete('cascade');
+
+            // 2nd method
+            $table->foreignId('camp_id')->constrained();
+            $table->string('name');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('camp_benefits');
+    }
+}
