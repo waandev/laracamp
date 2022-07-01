cc74b54fc6fe599f8d3ae702f924f3e92463e590
Muhammad Aswan <muhaswan190897@gmail.com>
2022-07-01 11:23:08 +0800

    install & integration socialite

--- routes/web.php
+++ routes/web.php
@@ -1,5 +1,6 @@
 <?php
 
+use App\Http\Controllers\UserController;
 use Illuminate\Support\Facades\Route;
 
 /*
@@ -24,6 +25,10 @@ Route::get('checkout', function () {
 Route::get('success-checkout', function () {
     return view('success_checkout');
 })->name('success-checkout');
+
+// socialite routes
+Route::get('sign-in-google', [UserController::class, 'google'])->name('user.login.google');
+Route::get('auth/google/callback', [UserController::class, 'handleProviderCallback'])->name('user.login.callback');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
